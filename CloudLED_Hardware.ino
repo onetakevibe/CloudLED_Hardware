1d0bf5b169aa5295644b6d1f0d3222c1cebc8fa3
phs1234 <timemarket777@naver.com>
2020-12-04 20:26:51 +0900

    첫 커밋

---
+++ CloudLED_Hardware.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+
+#define TX 2
+#define RX 3
+
+SoftwareSerial bluetoothSerial(TX, RX);   // 블루투스 객체 선언
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  bluetoothSerial.begin(9600);
+}
+
+void loop() {
+  if (bluetoothSerial.available()) {
+      char message = bluetoothSerial.read();
+
+      Serial.println(message);
+  }
+}
